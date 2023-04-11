cc69b81f437840eb96614c2b781f6cc78288dab4
Manant243 <mananttyagi26@gmail.com>
2023-04-11 12:17:57 +0530

    Implemented Navbar and Sidebar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery, Icon } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+
+import { Sidebar } from '..';
+import useStyles from './styles';
 
 const Navbar = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
+
   return (
-    <div>Navbar</div>
+    <>
+      <AppBar position='fixed'>
+        <Toolbar className = {classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color = "inherit"
+              edge = "start"
+              style = {{outline : 'none'}}
+              onClick={() => {}}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )} 
+          <IconButton color = "inherit" sx = {{ml:1}} onClick={() => {}}>
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && 'Search...'}
+          <div>
+            {!isAuthenticated ? (
+              <Button color = "inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button color = "inherit" component={Link} to={`/profile/:id`} className={classes.linkButton} onClick={() => {}}>
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar 
+                  style = {{width : 30, height : 30}}
+                  alt = "Profile"
+                  src = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search...'}
+        </Toolbar>
+      </AppBar>
+      <div>
+        <nav className={classes.drawer}>
+              {isMobile ? (
+                <Drawer
+                  variant = "temporary"
+                  anchor = "left"
+                  open = {mobileOpen}
+                  classes = {{ paper : classes.drawerPaper}}
+                  ModalProps={{KeepMounted : true}}
+                >
+                  <Sidebar setMobileOpen = {setMobileOpen} />
+                </Drawer>
+              ) : (
+                <Drawer classes={{paper : classes.drawerPaper}} >
+                  <Sidebar setMobileOpen = {setMobileOpen} />
+                </Drawer>
+              )}
+        </nav>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/styles';  
+
+import useStyles from './styles'
+
+const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
+const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
+
+const Sidebar = ({ setMobileOpen }) => {
+    const theme = useTheme();
+    const classes = useStyles();
+
+    return (
+        <>
+            <Link to = "/" className={classes.imageLink}>
+                <img 
+                    className={classes.image}
+                    src={theme.pallete.mode === 'light' ? redLogo : blueLogo}
+                    alt = "Philmy logo"
+                />
+            </Link>
+        </>
+    );
+};
+
+export default Sidebar; 
